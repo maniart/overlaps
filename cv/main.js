94d2f575118a7f485631bdd9d8bcf7640adb061e
maniart <mani.art@gmail.com>
2015-09-26 14:39:12 -0400

    WIP diff works

--- cv/main.js
+++ cv/main.js
@@ -1,10 +1,21 @@
 'use strict';
 
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
 var cameraOutput = document.querySelector('#camera-output');
 var rawImage = document.querySelector('#raw-image');
 var rawImageCtx = rawImage.getContext('2d');
 var diffImage = document.querySelector('#diff-image');
-var diffImageCrx = diffImage.getContext('2d');
+var diffImageCtx = diffImage.getContext('2d');
 var lastImageData;
 
 function captureFromCamera(output) {
@@ -25,12 +36,58 @@ function drawRawImage(cameraInput, outputCtx) {
     outputCtx.drawImage(cameraInput, 0, 0, cameraInput.width, cameraInput.height);
 }
 
+function mirror(canvas) {
+    var ctx = canvas.getContext('2d');
+    ctx.translate(canvas.width, 0);
+    ctx.scale(-1, 1);
+}
+
+function abs(value) {
+    // funky bitwise, equals Math.abs
+    return (value ^ (value >> 31)) - (value >> 31);
+}
+
+function threshold(value) {
+    return (value > 0x15) ? 0xFF : 0;
+}
+
+function diff(output, input1, input2, accuracy) {
+    if(input1.length  !== input2.length) return;
+    var i = 0;
+    var length = input1.length;
+    var average1;
+    var average2;
+    var diff;
+    while(i < length * accuracy) {
+        average1 = (input1[i*4] + input1[i*4+1] + input1[i*4+2]) / 2.5;
+        average2 = (input2[i*4] + input2[i*4+1] + input2[i*4+2]) / 2.5;
+        diff = threshold(abs(average1 - average2));
+        output[i*4] = diff;
+        output[i*4+1] = diff;
+        output[i*4+2] = diff;
+        output[i*4+3] = 0xFF;
+        ++i;
+    }
+}
+
 function blend(inputCtx, outputCtx, width, height) {
     var sourceData = inputCtx.getImageData(0, 0, width, height);
     var blendedData;
     if(!lastImageData) lastImageData = inputCtx.getImageData(0, 0, width, height);
     blendedData = inputCtx.createImageData(width, height);
-    diff(blendedData.data, sourceData, lastImageData, 2.5);
+    diff(blendedData.data, sourceData.data, lastImageData.data, 0.25);
     outputCtx.putImageData(blendedData, 0, 0);
     lastImageData = sourceData;
+}
+
+function loop() {
+    drawRawImage(cameraOutput, rawImageCtx);
+    blend(rawImageCtx, diffImageCtx, 320, 240);
+    requestAnimFrame(loop);
+}
+
+function init() {
+    mirror(rawImage);
+    captureFromCamera(cameraOutput);
+    loop()
 }
